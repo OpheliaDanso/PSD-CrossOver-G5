f55c40984446dd1ded0fadea7c49a96f38b0f72e
Renke Brixel <93048457+ReynkeDeVos@users.noreply.github.com>
2024-05-13 15:50:30 +0200

    Refactor component imports and add Page03

--- src/Components/PageOne.jsx
+++ src/Components/PageOne.jsx
@@ -1,5 +1,4 @@
 import first from "../assets/01.webp";
-import React from "react";
 
 export default function PageOne() {
   return (
@@ -10,19 +9,14 @@ export default function PageOne() {
         backgroundRepeat: "no-repeat",
         backgroundSize: "cover",
         // height: 1000,
-      }}
-    >
-      <h2 className="flex justify-center text-white pt-28 text-4xl items-start">
-        plants lover & co.
-      </h2>
+      }}>
+      <h2 className="flex justify-center text-white pt-28 text-4xl items-start">plants lover & co.</h2>
       <h2 className="flex justify-center text-white font-bold text-8xl items-start">
         HOUSE
         <br />
         PLANTS
       </h2>
-      <h2 className="flex justify-center text-white text-5xl items-start">
-        care guide
-      </h2>
+      <h2 className="flex justify-center text-white text-5xl items-start">care guide</h2>
       <h2 className="text-white pl-20 pt-40 text-3xl">
         Easy, hassle-free, steps to ensure that your
         <br />
