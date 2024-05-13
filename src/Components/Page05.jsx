68492d333b0398dda1852eb5f4da553bc782c671
Renke Brixel <93048457+ReynkeDeVos@users.noreply.github.com>
2024-05-13 15:48:00 +0200

    adding Page03

--- src/Components/Page05.jsx
+++ src/Components/Page05.jsx
@@ -10,14 +10,13 @@ const Page05 = () => {
         backgroundImage: `url('src/assets/05.webp')`,
         backgroundSize: "cover",
         backgroundPosition: "center",
-      }}
-    >
+      }}>
       <div className="absolute top-20 sm:left-20 left-4 flex flex-col items-start mt-10 sm:pt-10 sm:pl-4 pt-0 pl-0">
         <h1 className="text-9xl roboto-regular sm:text-custom-black md:text-custom-black text-white">
           Indoor
           <br /> plants
         </h1>
-        <p className="text-3xl fontroboto-light sm:text-custom-black md:text-custom-black lg:text-custom-black text-white mt-20 sm:w-1/2 md:w-full">
+        <p className="text-3xl roboto-light sm:text-custom-black md:text-custom-black lg:text-custom-black text-white mt-20 sm:w-1/2 md:w-full">
           Invite the nature into your home <br /> with these beautiful finds
         </p>
       </div>
