b3609fb36c02c1737769b543b72890f73b37e544
OpheliaDanso <mayaafreh@gmail.com>
2024-05-13 15:00:27 +0200

    change of name

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import PageOne from "./components/page 1";
 import PageTwo from "./components/page 2";
-import ComponentSeven from "./components/yo";
+import ComponentSeven from "./components/ComponentSeven";
 
 function App() {
   return (
