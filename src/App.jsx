caa531bb61919c891d8fb70535eb1e436ab2da23
Renke Brixel <93048457+ReynkeDeVos@users.noreply.github.com>
2024-05-13 15:43:09 +0200

    add Page03

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import Page8 from "./Components/Page8";
-import Page9 from "./Components/Page9";
+import Page03 from "./Components/Page03";
+// import Page04 from "./components/Page04";
+// import Page05 from "./components/Page05";
+// import Page8 from "./Components/Page8";
+// import Page9 from "./Components/Page9";
 
 function App() {
   return (
     <>
+      <Page03 />
+      {/* <Page04 />
+      <Page05 />
       <Page8 />
-      <Page9 />
+      <Page9 /> */}
     </>
   );
 }
