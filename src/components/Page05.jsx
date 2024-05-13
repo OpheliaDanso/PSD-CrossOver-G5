491c2dda8a709dafc9e3076c1ecdeb4a20128084
OpheliaDanso <mayaafreh@gmail.com>
2024-05-13 15:41:38 +0200

    correction of components

--- src/components/Page05.jsx
+++ src/components/Page05.jsx
@@ -1,6 +1,6 @@
 import "../roboto-font.css";
 
-export const Page05 = () => {
+const Page05 = () => {
   return (
     <div
       className="relative flex items-center justify-center"
@@ -10,7 +10,8 @@ export const Page05 = () => {
         backgroundImage: `url('src/assets/05.webp')`,
         backgroundSize: "cover",
         backgroundPosition: "center",
-      }}>
+      }}
+    >
       <div className="absolute top-20 sm:left-20 left-4 flex flex-col items-start mt-10 sm:pt-10 sm:pl-4 pt-0 pl-0">
         <h1 className="text-9xl roboto-medium sm:text-custom-black md:text-custom-black text-white">
           Indoor
@@ -23,3 +24,5 @@ export const Page05 = () => {
     </div>
   );
 };
+
+export default Page05;
